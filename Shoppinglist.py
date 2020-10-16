54724d2f7097dbf01df7527a25763e3d603fde0c
Adrian <63692318+adriand1az@users.noreply.github.com>
2020-10-15 19:35:14 -0700

    Update Shoppinglist.py

--- Shoppinglist.py
+++ Shoppinglist.py
@@ -1,38 +1,40 @@
-shopping_list = []
-
-def help():
-  print("What should we pick up out of the store")
-  print("""
-  Enter 'DONE' to stop adding items
-  Enter 'HELP' to see this menu again 
-  Enter 'LIST' to see your list
-  """)
+class ShoppingList():
+  shopping_list = []
   
-  
-def add_to_list(item):
-  shopping_list.append(item.capitalize())
-  print(f"Total Items: {len(shopping_list)}")
-
-  
-def show_list():
-  current_item = 1
-  for item in shopping_list:
-    print(f"{current_item}. {item}")
-    current_item += 1
-        
-help()     
-while True:  
-  new_item = input("Enter Item: ")
-  
-  if new_item == "DONE":
-    break  
-  elif new_item == "HELP":
-    help()
-    continue    
-  elif new_item == "LIST":
-    show_list()
-    continue
+  def help(self):
+    print("What should we pick up out of the store")
+    print("""
+    Enter 'DONE' to stop adding items
+    Enter 'HELP' to see this menu again 
+    Enter 'LIST' to see your list
+    """)
     
-  add_to_list(new_item)
+    
+  def add_to_list(self,item):
+    self.shopping_list.append(item.capitalize())
+    print(f"Total Items: {len(self.shopping_list)}")
   
-show_list()
+    
+  def show_list(self):
+    current_item = 1
+    for item in self.shopping_list:
+      print(f"{current_item}. {item}")
+      current_item += 1
+          
+instance = ShoppingList()
+instance.help()
+while True:  
+    new_item = input("Enter Item: ")
+    
+    if new_item == "DONE":
+      break  
+    elif new_item == "HELP":
+      instance.help()
+      continue    
+    elif new_item == "LIST":
+      instance.show_list()
+      continue
+      
+    instance.add_to_list(new_item)
+    
+instance.show_list()
